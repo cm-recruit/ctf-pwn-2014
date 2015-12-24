9e68867fe0ba185ede60710bc30aafd93df92842
cm-recruit <cm-recruit@users.noreply.github.com>
2015-12-23 19:34:22 -0800

    delete

--- plaid-ctf-2014/mtpox/index.php
+++
@@ -1,9 +0,0 @@
-<?
-  if (isset($_GET['page'])) {
-    if (strstr($_GET['page'], "secrets")) { echo "ERROR!\n"; }
-    else { readfile(basename($_GET['page'])); }
-  }
-  else {
-    readfile("index");
-  }
-?>
